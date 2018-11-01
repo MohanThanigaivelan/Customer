48087c0f2728b67047f79687cafbb8f232258054
Rently <rently@Rentlys-MacBook-Air.local>
2018-11-01 14:43:20 +0530

    Devise initialize

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :customer1s
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    
    resources :customers, :orders ,:items
